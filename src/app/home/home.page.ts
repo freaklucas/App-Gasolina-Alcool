718a85cacbc849b4e6341034939b8fd745494e8c
freaklucas <lucas.oliveira.freak@gmail.com>
2020-08-15 20:57:54 -0300

    versão inicial do app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public resultado: String="Resultado"
+  /* inicializando as var p conectar elas ao ngModel */
+  public precoAlcool: string =""
+  public precoGasolina: string =""
+
+  public calcular() {
+    /* validar se os campos foram preenchidos */
+    if(this.precoAlcool && this.precoGasolina) {
+
+      var pAlcool = parseFloat(this.precoAlcool);
+      var pGasolina = parseFloat(this.precoGasolina);
+
+      /* Lógica: if pGasolina * 0,7 < pALcool : coloque alcool */
+      var res = pAlcool / pGasolina
+
+      if(res >= 0.7) {
+        this.resultado = "Melhor utilizar Gasolina"
+      }
+      else {
+        this.resultado = "Melhor utilizar Álcool"
+      }
+
+    }
+    else {
+      this.resultado ="Preencha todos os dois campos corretamente!"
+    }
+  }
 
   constructor() {}
 
 }
+
+
